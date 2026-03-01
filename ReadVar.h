0e7fff4bdc9f92c71d54674a870d2bf051a447a5
qeqeqecode533 <namelametwo@gmail.com>
2026-02-28 20:53:37 -0700

    Refactor ReadVar.h to include input reading functions

--- ReadVar.h
+++ ReadVar.h
@@ -1,48 +1,37 @@
 #ifndef READVAR_H
 #define READVAR_H
 
-#include <iostream> // Added for pt and ptl
 #include <string>
+#include <limits>
 
-// === Output functions ===
-template <typename T> void pt(T msg) {
-    std::cout << msg;
+using ll = long long;
+namespace readvar {
+    int readInt(int min, int max, const std::string& prompt, const std::string& reprompt);
+    int readInt(int min, const std::string& prompt, const std::string& reprompt);
+    int readInt(const std::string& prompt, const std::string& reprompt);
+    int readInt(const std::string& prompt);
+    int readInt();
+    ll readLongLong(ll min, ll max, const std::string& prompt, const std::string& reprompt);
+    ll readLongLong(ll min, const std::string& prompt, const std::string& reprompt);
+    ll readLongLong(const std::string& prompt, const std::string& reprompt);
+    ll readLongLong(const std::string& prompt);
+    ll readLongLong();
+    float readFloat(float min, float max, const std::string& prompt, const std::string& reprompt);
+    float readFloat(float min, const std::string& prompt, const std::string& reprompt);
+    float readFloat(const std::string& prompt, const std::string& reprompt);
+    float readFloat(const std::string& prompt);
+    float readFloat();
+    double readDouble(double min, double max, const std::string& prompt, const std::string& reprompt);
+    double readDouble(double min, const std::string& prompt, const std::string& reprompt);
+    double readDouble(const std::string& prompt, const std::string& reprompt);
+    double readDouble(const std::string& prompt);
+    double readDouble();
+    std::string readString(const std::string& prompt, const std::string& reprompt);
+    std::string readString(const std::string& prompt);
+    std::string readString();
+    bool readBool(const std::string& prompt, const std::string& reprompt);
+    bool readBool(const std::string& prompt);
+    bool readBool();
 }
-inline void pt(const std::string &msg) {
-    std::cout << msg;
-}
-inline void pt(const char *msg) {
-    std::cout << msg;
-}
-
-template <typename T> void ptl(T msg) {
-    std::cout << msg << std::endl;
-}
-inline void ptl(const std::string &msg) {
-    std::cout << msg << std::endl;
-}
-inline void ptl(const char *msg) {
-    std::cout << msg << std::endl;
-}
-
-// === Input reading functions ===
-std::string readLine(const std::string &prompt = "", std::string reprompt = "");
-
-int readInt(int min, int max, const std::string &prompt = "", std::string reprompt = "");
-int readInt(int min, const std::string &prompt = "", const std::string &reprompt = "");
-int readInt(const std::string &prompt = "", const std::string &reprompt = "");
-
-double readDouble(double min, double max, const std::string &prompt = "", std::string reprompt = "");
-double readDouble(double min, const std::string &prompt = "", const std::string &reprompt = "");
-double readDouble(const std::string &prompt = "", const std::string &reprompt = "");
-
-float readFloat(float min, float max, const std::string &prompt = "", std::string reprompt = "");
-float readFloat(float min, const std::string &prompt = "", const std::string &reprompt = "");
-float readFloat(const std::string &prompt = "", const std::string &reprompt = "");
-
-bool equalsIgnoreCase(const std::string &a, const std::string &b);
-bool readBool(const std::string &prompt = "", std::string reprompt = "");
-
-char readChar(const std::string &prompt = "", std::string reprompt = "");
 
 #endif // READVAR_H
